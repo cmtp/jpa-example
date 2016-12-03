76ac4027ad89f88ab0070106a775847356d0ef20
cmtp <christian@hotmail.com>
2016-12-03 11:31:37 -0400

    add testing

--- src/main/java/com/example/test/TestEmployees.java
+++ src/main/java/com/example/test/TestEmployees.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 /**
@@ -23,7 +24,24 @@ public class TestEmployees {
         emf = Persistence.createEntityManagerFactory("Persistence");
         manager = emf.createEntityManager();
 
-        List<Employee> employees = (List<Employee>) manager.createQuery("FROM Employee").getResultList();
-        System.out.println("In this database exists " + employees.size() + " employees.");
+        Employee e = new Employee(10L, "Pepito", "Perez", new GregorianCalendar(1979, 6, 6).getTime());
+        Employee e2 = new Employee(25L, "Jose", "Perez", new GregorianCalendar(1984, 6, 6).getTime());
+        Employee e3 = new Employee(30L, "Juan", "Perez", new GregorianCalendar(2000, 6, 6).getTime());
+        manager.getTransaction().begin();
+        manager.persist(e);
+        manager.persist(e2);
+        manager.persist(e3);
+        manager.getTransaction().commit();
+
+        printAll();
+    }
+
+    @SuppressWarnings("unchecked")
+    private static void printAll() {
+        List<Employee> emps = (List<Employee>) manager.createQuery("FROM Employee").getResultList();
+        System.out.println("Exist " + emps.size() + " employees in the system.");
+        for (Employee emp: emps) {
+            System.out.println(emp.toString());
+        }
     }
 }
